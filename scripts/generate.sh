1e5d439a37685f49b569b904c91fcb8f983571df
Jordan Acosta <jordanjacosta@gmail.com>
2024-01-04 15:04:55 -0800

    fix: use docker to run openapi-generator
    Easiest way to "install" it in the GA runner.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
-#SPEC=https://ctl.prod.nuon.co/oapi/v3
-SPEC=https://ctl.stage.nuon.co/oapi/v3
+SPEC=https://ctl.prod.nuon.co/oapi/v3
+#SPEC=https://ctl.stage.nuon.co/oapi/v3
 #SPEC=http://localhost:8081/oapi/v3
 
-openapi-generator generate -i $SPEC -g elixir -o ./
+#openapi-generator generate -i $SPEC -g elixir -o ./
+docker run --rm \
+  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+  -i $SPEC \
+  -g elixir \
+  -o /local
