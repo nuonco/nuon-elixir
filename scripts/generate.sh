9c9c3bb2a03e67addc50ded0b1b137282ecfbdff
Jordan Acosta <jordanjacosta@gmail.com>
2024-01-08 15:29:18 -0800

    fix: add license and links
    Required to publish to Hex. Had to override template to get the output I
    wanted.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,13 +2,14 @@
 
 SPEC=https://ctl.prod.nuon.co/oapi/v3
 #SPEC=https://ctl.stage.nuon.co/oapi/v3
-#SPEC=http://localhost:8081/oapi/v3
+#SPEC=http://host.docker.internal:8081/oapi/v3
 
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i $SPEC \
   -g elixir \
-  -o /local
+  -o /local \
+  -t /local/scripts/templates
 
 # save version to file for workflows to read from
 cat mix.exs | grep version | sed -e 's/.*version: "\(.*\)",/\1/' > version.txt
